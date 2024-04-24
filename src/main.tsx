2d68a1d4bf90b38828ebe85c6fe0b3bac436690a
ad891912 <ad891912@ens.uqam.ca>
2024-04-24 11:09:02 -0400

    AJOUT reset.scss et utilise normalize.scss du dossier sass plutot que de Node

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
-import 'normalize.css' // important: import before any other css
+// import 'normalize.css' // important: import before any other css
 // import 'bootstrap/dist/css/bootstrap.css'
 import router from './routing/routes'
 import PageTitle from './components/PageTitle';
