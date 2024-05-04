8480a5065af168e71bfeb6920a3910dd67053ae8
ad891912 <ad891912@ens.uqam.ca>
2024-05-04 11:10:08 -0400

    MODIF ajout glitch sur ErrorPage.tsx

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,7 @@
-import {isRouteErrorResponse, useRouteError} from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 import PageTitle from "../components/PageTitle";
 import Footer from "../layout/Footer";
+import Glitch from "../components/glitch";
 
 const ErrorPage = () => {
   // const error = useRouteError();
@@ -10,19 +11,19 @@ const ErrorPage = () => {
   let errorName = isRouteErrorResponse(useRouteError())
     ? 'Erreur 404: Page inexistante'
     : 'Erreur inattendue'
-  
+
   return (
-    <>      
+    <>
       <PageTitle noSiteName>{errorName}</PageTitle>
 
       <div className="bigdot">
         <div className="errortextbloc">
 
-          <h1>Oups...</h1>
-          <h4>
-            {errorName}
-          </h4>
-
+          <Glitch><h1>Oups...</h1>
+            <h4>
+              {errorName}
+            </h4>
+          </Glitch>
 
         </div>
         <Footer />
