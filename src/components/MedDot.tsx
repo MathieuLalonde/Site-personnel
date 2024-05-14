5cb586dc2d5b8cacaa446d6a2c844c8fe1c4e84d
ad891912 <ad891912@ens.uqam.ca>
2024-05-14 13:54:12 -0400

    MODIF ajout de aria-hidden au meddot

--- src/components/MedDot.tsx
+++ src/components/MedDot.tsx
@@ -7,7 +7,8 @@ interface Props {
 
 const MedDot = ({ className, size='35rem' }: Props) => {
   return (
-    <img src={Dot} width={`${size}`} height={`${size}`} className={`meddot ${className}`} style={{ margin: '4rem auto' }} />
+    <img src={Dot} width={`${size}`} height={`${size}`} className={`meddot ${className}`} style={{ margin: '4rem auto' }}
+      aria-hidden="true"/>
   )
 }
 
