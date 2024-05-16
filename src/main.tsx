e6338d872b5123f2e0e11b2c2a3c712e7ce3b271
ad891912 <ad891912@ens.uqam.ca>
2024-05-16 10:41:13 -0400

    MODIF cleanup general

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
-// import 'normalize.css' // important: import before any other css
-// import 'bootstrap/dist/css/bootstrap.css'
 import router from './layout/routes'
 import PageTitle from './components/PageTitle';
 import './sass/main.scss'
