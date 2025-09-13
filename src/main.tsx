e6e2cb2e821bbbd7adce57da95a3feca7d26c572
Mathieu Lalonde <74269710+MathieuLalonde@users.noreply.github.com>
2025-09-13 18:51:12 -0400

    feat: added changing favicon by environment and .env files

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,18 @@ import router from './layout/routes'
 import PageTitle from './components/PageTitle';
 import './sass/main.scss'
 
+const setFavicon = (iconPath: string) => {
+  let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement | null;
+  if (!link) {
+    link = document.createElement("link");
+    link.rel = "icon";
+    document.head.appendChild(link);
+  }
+  link.href = iconPath;
+};
+
+setFavicon(import.meta.env.VITE_FAVICON);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <PageTitle description='Le site web de Mathieu Lalonde, developpeur web/logiciel et monteur télévisuel'></PageTitle>
